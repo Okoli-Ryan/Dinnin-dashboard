4a00031dbd1c687eabc691fddee6fe82f92831e7
okoli-ryan <okoliryan50@gmail.com>
2023-04-10 15:06:51 +0100

    redux tk store

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import "react-phone-input-2/lib/style.css";
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
