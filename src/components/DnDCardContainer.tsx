8935f6ee81b9cd0961194497c84b96e11837239d
okoli-ryan <okoliryan50@gmail.com>
2023-06-05 13:20:52 +0100

    implemented sorting

--- src/components/DnDCardContainer.tsx
+++ src/components/DnDCardContainer.tsx
@@ -16,52 +16,23 @@ export const DndCardContainer: FC<IDndCardContainer> = ({ index, moveCard, child
 	const ref = useRef<HTMLDivElement>(null);
 	const [{ handlerId }, drop] = useDrop<DragItem, void, { handlerId: Identifier | null }>({
 		accept,
-		collect(monitor) {
-			return {
-				handlerId: monitor.getHandlerId(),
-			};
-		},
+		// collect(monitor) {
+		// 	return {
+		// 		handlerId: monitor.getHandlerId(),
+		// 	};
+		// },
 		hover(item: DragItem, monitor) {
 			if (!ref.current) {
 				return;
 			}
+
+			console.log({ item, index });
+
 			const dragIndex = item.index;
 			const hoverIndex = index;
 
 			// Don't replace items with themselves
 			if (dragIndex === hoverIndex) {
-				return;
-			}
-
-			// Determine rectangle on screen
-			const hoverBoundingRect = ref.current?.getBoundingClientRect();
-
-			// Get vertical middle
-			const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-			//Get horizontal middle
-			const hoverMiddleX = hoverBoundingRect.right - hoverBoundingRect.left;
-
-			// Determine mouse position
-			const clientOffset = monitor.getClientOffset();
-
-			// Get pixels to the top
-			const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
-
-			//Get pixels to the left
-			const hoverClientX = (clientOffset as XYCoord).x - hoverBoundingRect.left;
-
-			// Only perform the move when the mouse has crossed half of the items height
-			// When dragging downwards, only move when the cursor is below 50%
-			// When dragging upwards, only move when the cursor is above 50%
-
-			// Dragging downwards
-			if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
-				return;
-			}
-
-			// Dragging upwards
-			if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
 				return;
 			}
 
@@ -72,7 +43,7 @@ export const DndCardContainer: FC<IDndCardContainer> = ({ index, moveCard, child
 			// Generally it's better to avoid mutations,
 			// but it's good here for the sake of performance
 			// to avoid expensive index searches.
-			item.index = hoverIndex;
+			item.index = index;
 		},
 	});
 
